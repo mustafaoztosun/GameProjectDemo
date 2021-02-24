04546c042df3cf3c5de4f7463d7cdc23681cf264
16 Ocak Ders Sonu <mustafaoztosun.g@gmail.com>
2021-02-24 20:39:14 +0300

    Add project files.

---
+++ GameProjectDemo/GamerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProjectDemo
+{
+    class GamerManager
+    {
+        public void Add(Gamer gamer1)
+        {
+            Console.WriteLine("Tebrikler! Yeni Oyuncu Eklendi...", gamer1);
+        }
+        
+        public void Update(Gamer gamer2)
+        {
+            Console.WriteLine("Tebrikler! Oyuncu Yükseltildi...", gamer2);
+        }
+
+        public void Delete(Gamer gamer3)
+        {
+            Console.WriteLine("Oyuncu Silme İşlemi Başarılı...",gamer3);
+        }
+
+
+        
+
+    }
+}
---
+++ GameProjectDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GameProjectDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Gamer gamer1 = new Gamer();
+            gamer1.Id = 1;
+            gamer1.FirstName = "Mustafa";
+            gamer1.LastName = "Öztosun";
+            gamer1.TcNo = "12345678910";
+
+            Gamer gamer2 = new Gamer();
+            gamer2.Id = 2;
+            gamer2.FirstName = "İsmail";
+            gamer2.LastName = "Akçuru";
+            gamer2.TcNo = "10987654321";
+
+            Gamer gamer3 = new Gamer();
+            gamer3.Id = 3;
+            gamer3.FirstName = "Salih";
+            gamer3.LastName = "Avcı";
+            gamer3.TcNo = "25469873124";
+
+            Gamer[] gamers = new Gamer[] { gamer1, gamer2, gamer3 };
+
+            foreach (var gamer in gamers)
+            {
+                Console.WriteLine("*************Oyuncu Bilgileri*************");
+                Console.WriteLine(gamer.Id);
+                Console.WriteLine(gamer.FirstName);
+                Console.WriteLine(gamer.LastName);
+                Console.WriteLine(gamer.TcNo);
+                Console.WriteLine("-----------------------------------------------------");
+            }
+
+
+        }
+    }
+}
